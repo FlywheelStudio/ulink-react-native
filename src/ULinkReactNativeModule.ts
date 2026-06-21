d3f7f65b3746fa72492beb463c0495c65d58d921
Mohanned Binmiskeen <mohn93@gmail.com>
2026-06-21 16:26:28 +0200

    chore: remove dead create-expo-module scaffold files (superseded by ULinkModule.ts)

--- src/ULinkReactNativeModule.ts
+++
@@ -1,5 +0,0 @@
-import { NativeModule, requireNativeModule } from 'expo';
-
-declare class ULinkReactNativeModule extends NativeModule<{}> {}
-
-export default requireNativeModule<ULinkReactNativeModule>('ULinkReactNative');
--- src/ULinkReactNativeModule.web.ts
+++
@@ -1,6 +0,0 @@
-import { registerWebModule, NativeModule } from 'expo';
-
-// ULinkReactNativeModule is not available on the web platform.
-class ULinkReactNativeModule extends NativeModule<{}> {}
-
-export default registerWebModule(ULinkReactNativeModule, 'ULinkReactNativeModule');
